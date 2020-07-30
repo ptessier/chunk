82eaae68d012267766fc61a42cbf58755a20e2f4
Philippe Tessier <philipper.tessier@gmail.com>
2020-07-30 19:07:30 -0400

    🎨 add `tslint` and `prettier`

--- src/helper/environment.ts
+++ src/helper/environment.ts
@@ -1,8 +1,7 @@
 export const isProduction = () => {
-    return process.env.NODE_ENV === 'production';
+  return process.env.NODE_ENV === 'production';
 };
 
 export const isDevelopment = () => {
-    return process.env.NODE_ENV !== 'production';
+  return process.env.NODE_ENV !== 'production';
 };
-  
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
-import { log } from './logger';
 import { createServer } from './create-server';
+import { log } from './logger';
 
 const typeDefs = `
   type Query {
     hello(name: String): String!
   }
-`
+`;
 
 const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || 'World'}`,
   },
-}
-
-const options = {
-    port: 5000,
-    playground: '/playground'
 };
 
-const server = createServer({ typeDefs, resolvers })
+const options = {
+  port: 5000,
+  playground: '/playground',
+};
+
+const server = createServer({ typeDefs, resolvers });
 
 log.info('Starting server.');
 
 server.start(options, () => {
   log.info(`Server is running on http://localhost:${options.port}`);
 
-  if(options.playground) {
+  if (options.playground) {
     log.info(`GraphQL playground is running on http://localhost:${options.port}${options.playground}`);
   }
 });
