064fb7ecf098f719aea3be0ab442fffdbe68e763
Philippe Tessier <philipper.tessier@gmail.com>
2020-07-30 20:46:02 -0400

    :heavy_plus_sign: add `cors`, `compression` and `helmet` support

--- src/create-server.ts
+++ src/create-server.ts
@@ -1,14 +1,35 @@
+import compression, { CompressionOptions } from 'compression';
+import cors, { CorsOptions } from 'cors';
 import pino from 'express-pino-logger';
 import { GraphQLServer } from 'graphql-yoga';
 import { Props } from 'graphql-yoga/dist/types';
+import helmet, { IHelmetConfiguration } from 'helmet';
 import { log } from '~/logger';
 
 export interface Options extends Props {
+  compression?: boolean | CompressionOptions;
+  cors?: boolean | CorsOptions;
+  helmet?: boolean | IHelmetConfiguration;
   logging?: boolean;
 }
 
 export function createServer(options: Options): GraphQLServer {
   const server: GraphQLServer = new GraphQLServer(options);
+
+  if (options.compression) {
+    const compressionOptions = typeof options.compression === 'boolean' ? {} : options.compression;
+    server.use(compression(compressionOptions));
+  }
+
+  if (options.cors) {
+    const corsOptions = typeof options.cors === 'boolean' ? {} : options.cors;
+    server.use(cors(corsOptions));
+  }
+
+  if (options.helmet) {
+    const helmetOptions = typeof options.helmet === 'boolean' ? {} : options.helmet;
+    server.use(helmet(helmetOptions));
+  }
 
   if (options.logging) {
     server.use(pino({ logger: log }));
